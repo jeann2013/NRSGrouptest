33a4c8fa08586e05f049c976d2786e4559bd1406
Jean C. Nunez <jeancarlosn2008@gmail.com>
2019-01-08 21:01:24 -0500

    Modulos de Usuarios, controller y rutas

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+    <br>
+    <br>
+    <br>
+    <div class="container">
+        <div align="center" class="row">
+            <div align="center" class="col-sm">
+                <table id="views" class="table" >
+                    <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Correo</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Auth::routes(['verify'=> true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+/**Users**/
+Route::get('/users', 'UsersController@index')->name('users');
+Route::get('/users/add', 'UsersController@add')->name('users/add');
+Route::post('/users/new', 'UsersController@news')->name('users/new');
+Route::get('/users/edit/{id}', 'UsersController@edit');
+Route::post('/users/update', 'UsersController@update');
+Route::get('/users/delete/{id}', 'UsersController@delete');
+Route::post('/users/destroy', 'UsersController@destroy');
+/**Users**/
